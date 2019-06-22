18fc1995738e0c002be81c62b1d40013bc6c8944
123 <123456>
2019-06-22 20:50:06 +0800

    2131

---
+++ git_test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git properties");
+        System.out.println("hello git properties");
+        System.out.println("hello git properties");
+        System.out.println("hello git properties");
+        System.out.println("hello git properties");
+        System.out.println("hello git properties");
+        System.out.println("hello git properties");
+    }
+}
